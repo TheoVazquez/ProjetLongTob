5fa6dc6e08435d61d951609e7fac59303d423e6b
AymenBenAbdallah <49407505+AymenBenAbdallah@users.noreply.github.com>
2019-04-10 18:43:31 +0200

    Update Competence.java

--- Competence.java
+++ Competence.java
@@ -1,1 +1,9 @@
-
+public class Competence {
+	private String nom; //nom de la Compétence
+	private int valeur; //sa valeur
+	
+	public Competence(String nom, int valeur) {
+		this.nom = nom;
+		this.valeur = valeur;
+	}
+}
