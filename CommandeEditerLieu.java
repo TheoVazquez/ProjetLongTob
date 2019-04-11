1e140a88bee8ccd15d2b4b8d07d450ad0248d376
Spilka--georgeot Basile <bspilkag@localhost.localdomain>
2019-04-11 15:56:39 +0200

    modifié :         CommandeEditerLieu.java 	modifié :         CommandeEditerScenario.java

--- CommandeEditerLieu.java
+++ CommandeEditerLieu.java
@@ -1,9 +1,34 @@
+import java.util.Scanner;
 
 public class CommandeEditerLieu implements Commande {
-
+	
+	Scenario scenario;
+	
 	@Override
 	public void executer() {
-		// TODO Auto-generated method stub
+		
+	
+        Scanner sc = new Scanner(System.in);
+		
+			
+		System.out.println("Entrer le nom de la fiche a créer ou modifier : ");
+			
+		String nomFiche = sc.next();
+			
+		for (int i = 0; i < scenario.getListeLieux().size();i++) {
+				
+			if (scenario.getListeLieux().get(i).getNomLieu() == nomFiche) {
+					
+				scenario.getListeLieux().get(i).editer();
+				return;
+					
+			}
+		}
+			
+		System.out.println("la fiche n'existe pas creation de cette dernière...");
+		FicheLieu nouvelleFiche = new FicheLieu(nomFiche);
+	    scenario.ajouterLieu(nouvelleFiche);
+		return;
 
 	}
 
--- CommandeEditerScenario.java
+++ CommandeEditerScenario.java
@@ -1,4 +1,3 @@
-import java.util.Scanner;
 
 public class CommandeEditerScenario implements Commande {
 
@@ -11,43 +10,6 @@ public class CommandeEditerScenario implements Commande {
 	}
 	@Override
 	public void executer() {
-		
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.println("Voulez-vous modifier/créer une fiche lieu ? o/n ");
-		
-		if (sc.next() == "o") {
-			
-			System.out.println("Entrer le nom de la fiche a créer ou modifier : ");
-			
-			String nomFiche = sc.next();
-			
-			for (int i = 0; i < scenario.getListeLieux().size();i++) {
-				
-				if (scenario.getListeLieux().get(i).getNomLieu() == nomFiche) {
-					
-					scenario.getListeLieux().get(i).editer();
-					return;
-					
-				}
-			}
-			
-			System.out.println("la fiche n'existe pas creation de cette dernière...");
-			FicheLieu nouvelleFiche = new FicheLieu(nomFiche);
-	        scenario.ajouterLieu(nouvelleFiche);
-			return;	
-		
-		}
-		
-		else {
-			
-			System.out.println("Voulez vous modifier/créer une fiche personnage ? o/n" );
-			
-			////////////////////// TO DO ////////////////////////////////////////////////
-			
-		}
-			
-		
 		
 	}
 
