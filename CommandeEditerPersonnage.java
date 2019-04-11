4985adeac1247bb69015fc43f152e2d7feb4d868
Vazquez Theo <tvazquez@localhost.localdomain>
2019-04-11 09:46:23 +0200

    Sous menu

---
+++ CommandeEditerPersonnage.java
@@ -0,0 +1,16 @@
+
+public class CommandeEditerPersonnage implements Commande {
+
+	@Override
+	public void executer() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean estExecutable() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
