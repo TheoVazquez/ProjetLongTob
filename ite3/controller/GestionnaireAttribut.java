4a5d159c272ac755267c383dd12ff0348e4ce16a
Alex12AC <49405070+Alex12AC@users.noreply.github.com>
2019-05-21 15:25:27 +0200

    Add files via upload

---
+++ ite3/controller/GestionnaireAttribut.java
@@ -0,0 +1,25 @@
+package controller;
+
+import model.fiche.attribut.Attribut;
+
+public class GestionnaireAttribut<T> {
+
+	private Attribut<T> attribut;
+	
+	public GestionnaireAttribut(Attribut<T> attribut) {
+		this.attribut = attribut;
+	}
+	
+	public String getNomAttribut() {
+		return this.attribut.getNom();
+	}
+	
+	public T getValeurAttribut() {
+		return this.attribut.getValeur();
+	}
+	
+	public void setValeurAttribut(T valeur) {
+		this.attribut.setValeur(valeur);
+	}
+	
+}
